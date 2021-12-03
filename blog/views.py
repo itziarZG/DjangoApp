0a49050de9a88da33cec7dae03284d0991ca7514
itziarZG <izameza@apsl.net>
2021-12-03 13:08:47 +0100

    add styles and detail post

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
+from .models import Post
+from django.utils import timezone
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts=Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html',{'posts': posts})
+
+def post_detail(request, pk):
+    # post = Post.objects.get(pk=pk)
+    post=get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',  
 ]
 
 MIDDLEWARE = [
@@ -119,4 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+VENV_PATH = os.path.dirname(BASE_DIR)
+STATIC_ROOT = os.path.join(VENV_PATH, 'static_root')
